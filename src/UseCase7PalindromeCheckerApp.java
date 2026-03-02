8be88cbda6834980e11f8fc2aa74f5a25426bd63
Nirik-11 <ninirik11@gmail.com>
2026-03-02 10:53:23 +0530

    Add UC6

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class UseCase7PalindromeCheckerApp {
+    public static boolean isPalindrome(String str) {
+        if (str == null) return false;
+
+        // FIX 1: Added missing comma in replaceAll
+        String cleanStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : cleanStr.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            // Characters are compared as primitives, so != works fine here
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // FIX 2: Added missing commas between array elements
+        String[] testStrings = {
+                "racecar",
+                "A man, a plan, a canal: Panama",
+                "hello",
+                "Madam, I'm Adam"
+        };
+
+        for (String s : testStrings) {
+            System.out.println("\"" + s + "\" is palindrome: " + isPalindrome(s));
+        }
+    }
+}
